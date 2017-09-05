08c79b4908475bb878e183fa2f860db82c7d5882
Gonzalo Ramirez <gandres.ramirez@gmail.com>
2017-09-05 17:34:18 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# limitations under the License.
+import webapp2
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
